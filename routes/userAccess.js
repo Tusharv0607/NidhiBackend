12b903adf3a351fd4e6a7019806374ad4801ba0b
Tusharv0607 <2020110785@diu.iiitvadodara.ac.in>
2023-03-31 21:30:44 +0530

    added user access to fetch balance status

--- routes/userAccess.js
+++ routes/userAccess.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const fetchUser = require('../middleware/fetchUser');
+const User = require('../models/user')
+const Transactions = require('../models/transactions');
 const BankDetails = require('../models/bankdetails');
 const { body, validationResult } = require('express-validator');
 
@@ -26,7 +28,7 @@ router.post('/addBankDetails',
             }
 
             //Creating new note
-            const { AccHolderName,BankName, AccountNo, IFSC, Type } = req.body;
+            const { AccHolderName, BankName, AccountNo, IFSC, Type } = req.body;
             const details = new BankDetails({
                 userId: req.user.id,
                 AccHolderName,
@@ -44,7 +46,54 @@ router.post('/addBankDetails',
             res.status(500).json({ error: "Internal Server Error" });
         }
     });
-    
+//------------------------------------------------------------------------------//
+
+router.get('/balanceStatus',
+    fetchUser, // Middleware function to verify if the user is an admin
+    [
+        body('email', 'Enter a valid email').isEmail(), // Validates if Email is of correct format      
+    ],
+    async (req, res) => {
+        try {
+            // Validate request parameters using express-validator
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) { //If there are any validation errors return bad request error response
+                return res.status(400).json({ errors: errors.array() });
+            }
+
+            // Extracts email and amount from request body
+            const { email } = req.body;
+
+            // Finds user with given email
+            const user = await User.findOne({ email });
+
+            // Returns error response if user not found
+            if (!user) {
+                return res.status(400).json({ error: "Input correct crediantials" }); //return error response when user not found
+            }
+
+            // Finds transaction record associated with the user id and updates the allotted amount
+            const userId = user._id; //get the user Id of current user
+            const transaction = await Transactions.findOne({ userId }) //find transactions record for this user id
+
+            const data = {
+                allotedAmt: transaction.allotedAmt,
+                lockedAmt: transaction.lockedAmt,
+                availToWithdraw: transaction.availToWithdraw, //get different types of amount from transaction record
+                disburseAmt: transaction.disbursedAmt
+            }
+
+            // Returns success response with amounts details
+            res.status(200).json(data);
+        }
+        catch (error) {
+            console.error(error.message);
+
+            // Returns error response for internal server error
+            res.status(500).json({ error: "Internal Server Error" }); //return error response if there is any internal server error
+        }
+    });
+
 //------------------------------------------------------------------------------//
 
 module.exports = router;
